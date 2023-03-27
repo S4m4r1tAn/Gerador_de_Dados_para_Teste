d3f76b0cb796359df69ebb2af2f943cd34f0a795
S4m4r1t4N <emerson_guimaraes@hotmail.com>
2023-03-27 15:16:10 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,5 @@
+nomes = ['Emerson', 'Keli', 'Thomas', 'Nicholas', 'Christopher']
+email = ['emerson@email.com', 'keli@email.com', 'thomas@email.com', 'nicholas@email.com', 'christopher@email.com']
+telefone = [1143527995, 5199963652, 447909138487, 1143526669, 3499965889]
+cidade = ['Sao Bernado do Campo', 'Niteroi', 'Caratinga', 'Bonito', 'Brumado']
+estado = ['Sao Paulo', 'Rio de Janeiro', 'Minas Gerais', 'Mato Grasso', 'Bahia']
